b53462748a81472929e73ce4c0fb3867ecb98486
Nyombe <acholkuoldeng55@gmail.com>
2026-04-30 08:16:19 -0700

    Fix: Enable Tailwind build on Render using nodeenv

--- build.sh
+++ build.sh
@@ -1,7 +1,18 @@
 #!/usr/bin/env bash
+# exit on error
+set -o errexit
 
 echo "Installing Python dependencies..."
 pip install -r requirements.txt
+
+echo "Installing Node.js via nodeenv..."
+nodeenv -p --node=20.11.0
+
+echo "Installing Node dependencies..."
+npm install
+
+echo "Building Tailwind CSS..."
+npm run build
 
 echo "Collecting static files..."
 python manage.py collectstatic --no-input
