55ab28d9009e7a015228d082931fe29e3bb28916
Nyombe <acholkuoldeng55@gmail.com>
2026-05-08 01:21:12 -0700

    Upgrade Jazzmin admin theme with modern styling and custom CSS

--- core/admin.py
+++ core/admin.py
@@ -4,9 +4,9 @@ Core app admin configuration.
 from django.contrib import admin
 
 # Customize the default admin site
-admin.site.site_header = "🛒 Achol E-Commerce Admin"
-admin.site.site_title = "Achol Admin"
-admin.site.index_title = "Welcome to Achol Administration"
+admin.site.site_header = "Achol Fashion Store Management"
+admin.site.site_title = "Achol Management"
+admin.site.index_title = "Welcome to the Achol Admin Portal"
 
 
 
